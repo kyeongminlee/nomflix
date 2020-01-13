de47a071784f9a7825b6bb0ac03214810b974d7d
kyeongmin <ekmst1027@gmail.com>
2020-01-13 16:49:32 +0900

    #5.1 Container Presenter Pattern part Two

---
+++ src/Routes/Search/index.js
@@ -0,0 +1,4 @@
+import SearchContainer from "./SearchContainer";
+import SearchPresenter from "./SearchPresenter";
+
+export default SearchContainer;
---
+++ src/Routes/TV/TVContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import TVPresenter from "./TVPresenter";
+import { render } from "@testing-library/react";
+
+export default class extends React.Component {
+  state = {
+    topRated: null,
+    popular: null,
+    airingToday: null,
+    loading: true,
+    error: null
+  };
+
+  render() {
+    const { topRated, popular, airingToday, loading, error } = this.state;
+    return (
+      <TVPresenter
+        topRated={topRated}
+        popular={popular}
+        airingToday={airingToday}
+        loading={loading}
+        error={error}
+      />
+    );
+  }
+}
