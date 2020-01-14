bc57758bd7e0172dee4628b59704562e86c51d36
kyeongmin <ekmst1027@gmail.com>
2020-01-14 11:48:24 +0900

    #5.7 Destructuring assignment with let

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -31,13 +31,10 @@ export default class extends React.Component {
     let result = null;
     try {
       if (isMovie) {
-        const request = await moviesApi.movieDetail(parsedId);
-        result = request.data;
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
       } else {
-        const request = await tVApi.showDetail(parsedId);
-        result = request.data;
+        ({ data: result } = await tVApi.showDetail(parsedId));
       }
-      console.log(result);
     } catch {
       this.setState({ error: "Can't find anything." });
     } finally {
@@ -47,7 +44,7 @@ export default class extends React.Component {
 
   render() {
     const { result, error, loading } = this.state;
-    console.log(this.state);
+    console.log(result);
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
